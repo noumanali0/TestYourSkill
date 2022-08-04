05891045d4332f3462bf4040bcb8c6b499d774d9
noumanali0 <alinouman278@gmail.com>
2022-08-04 15:57:52 +0500

    populatedData

--- actions.js
+++ actions.js
@@ -3,6 +3,10 @@ const info_box = document.querySelector(".info_box");
 const exit_btn = document.querySelector(".quit");
 const cnt_btn = document.querySelector(".restart");
 const quiz_box = document.querySelector(".quiz_box");
+const que_text = document.querySelector(".que_text");
+const option_list = document.querySelector(".option_list");
+
+var question = [];
 
 start_btn.onclick = () => {
   info_box.classList.add("activeInfo");
@@ -12,12 +16,27 @@ exit_btn.onclick = () => {
   info_box.classList.remove("activeInfo");
 };
 
-cnt_btn.onclick = () => {
-  fetch("https://opentdb.com/api.php?amount=10")
-    .then((res) => res.json())
-    .then((rejson) => console.log(rejson.results))
-    .catch((err) => console.log(err));
-
+cnt_btn.onclick = async () => {
+  const res = await fetch("https://opentdb.com/api.php?amount=10");
+  const resJson = await res.json();
+  questions = resJson.results;
+  console.log(questions);
   info_box.classList.remove("activeInfo");
   quiz_box.classList.add("activeQuiz");
+  showQuestion(0);
+};
+
+const showQuestion = (index) => {
+  let question = `<span>${index + 1} . ${questions[index].question}  </span>`;
+  que_text.innerHTML = question;
+  const { incorrect_answers } = questions[index];
+  console.log(incorrect_answers);
+  const option =
+    questions[index].incorrect_answers.length == 1
+      ? `<div class="option"><span>${questions[index].incorrect_answers[0]}</span></div>`
+      : `<div class="option"><span>${questions[index].incorrect_answers[0]}</span></div> 
+        <div class="option"><span>${questions[index].incorrect_answers[1]}</span></div>
+        <div class="option"><span>${questions[index].incorrect_answers[2]}</span></div>`;
+
+  option_list.innerHTML = option;
 };
